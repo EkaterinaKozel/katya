352d0dbc90def8cb98ba5f0fdefbe37d72efce06
EkaterinaKozel <66666949+EkaterinaKozel@users.noreply.github.com>
2020-06-09 16:06:33 +0300

    Create Akinator.pl

---
+++ Akinator.pl
@@ -0,0 +1,243 @@
+%загаданный герой мужчина или женщина?
+%1 - Мужчина
+%2 - Женщина
+:- dynamic isMan/2
+%вопросы ...
+%описание персонажей
+:-assept(isMan("Гарри Поттер",1)).
+:-assept(fromGriffindor("Гарри Поттер",1)).
+:-assept(isDeathEater("Гарри Поттер",2)).
+:-assept(isAlive("Гарри Поттер",1)).
+:-assept(blood("Гарри Поттер",2)).
+:-assept(isHuman("Гарри Поттер",1)).
+
+:-assept(isMan("Гермиона Грейнджер",2)).
+:-assept(fromGriffindor("Гермиона Грейнджер",1)).
+:-assept(isDeathEater("Гермиона Грейнджер",2)).
+:-assept(isAlive("Гермиона Грейнджер",1)).
+:-assept(blood("Гермиона Грейнджер",3)).
+:-assept(isHuman("Гермиона Грейнджер",1)).
+
+:-assept(isMan("Рон Уизли",1)).
+:-assept(fromGriffindor("Рон Уизли",1)).
+:-assept(isDeathEater("Рон Уизли",2)).
+:-assept(isAlive("Рон Уизли",1)).
+:-assept(blood("Рон Уизли",1)).
+:-assept(isHuman("Рон Уизли",1)).
+
+:-assept(isMan("Сириус Блэк",1)).
+:-assept(fromGriffindor("Сириус Блэк",1)).
+:-assept(isDeathEater("Сириус Блэк",1)).
+:-assept(isAlive("Сириус Блэк",2)).
+:-assept(blood("Сириус Блэк",1)).
+:-assept(isHuman("Сириус Блэк",1)).
+
+:-assept(isMan("Джеймс Поттер",1)).
+:-assept(fromGriffindor("Джеймс Поттер",1)).
+:-assept(isDeathEater("Джеймс Поттер",2)).
+:-assept(isAlive("Джеймс Поттер",2)).
+:-assept(blood("Джеймс Поттер",1)).
+:-assept(isHuman("Джеймс Поттер",1)).
+
+:-assept(isMan("Лили Поттер",2)).
+:-assept(fromGriffindor("Лили Поттер",1)).
+:-assept(isDeathEater("Лили Поттер",2)).
+:-assept(isAlive("Лили Поттер",2)).
+:-assept(blood("Лили Поттер",3)).
+:-assept(isHuman("Лили Поттер",1)).
+
+:-assept(isMan("Минерва Макгонагалл",2)).
+:-assept(fromGriffindor("Минерва Макгонагалл",1)).
+:-assept(isDeathEater("Минерва Макгонагалл",2)).
+:-assept(isAlive("Минерва Макгонагалл",1)).
+:-assept(blood("Минерва Макгонагалл",2)).
+:-assept(isHuman("Минерва Макгонагалл",1)).
+
+:-assept(isMan("Северус Снегг",1)).
+:-assept(fromGriffindor("Северус Снегг",2)).
+:-assept(isDeathEater("Северус Снегг",1)).
+:-assept(isAlive("Северус Снегг",2)).
+:-assept(blood("Северус Снегг",2)).
+:-assept(isHuman("Северус Снегг",1)).
+
+:-assept(isMan("Драко Малфой",1)).
+:-assept(fromGriffindor("Драко Малфой",2)).
+:-assept(isDeathEater("Драко Малфой",1)).
+:-assept(isAlive("Драко Малфой",1)).
+:-assept(blood("Драко Малфой",1)).
+:-assept(isHuman("Драко Малфой",1)).
+
+:-assept(isMan("Том Реддл",1)).
+:-assept(fromGriffindor("Том Реддл",2)).
+:-assept(isDeathEater("Том Реддл",1)).
+:-assept(isAlive("Том Реддл",2)).
+:-assept(blood("Том Реддл",2)).
+:-assept(isHuman("Том Реддл",1)).
+
+:-assept(isMan("Аластор Грюм",1)).
+:-assept(fromGriffindor("Аластор Грюм",1)).
+:-assept(isDeathEater("Аластор Грюм",2)).
+:-assept(isAlive("Аластор Грюм",2)).
+:-assept(blood("Аластор Грюм",1)).
+:-assept(isHuman("Аластор Грюм",1)).
+
+:-assept(isMan("Рубеус Хагрид",1)).
+:-assept(fromGriffindor("Рубеус Хагрид",1)).
+:-assept(isDeathEater("Рубеус Хагрид",2)).
+:-assept(isAlive("Рубеус Хагрид",1)).
+:-assept(blood("Рубеус Хагрид",5)).
+:-assept(isHuman("Рубеус Хагрид",2)).
+
+:-assept(isMan("Альбус Дамблдор",1)).
+:-assept(fromGriffindor("Альбус Дамблдор",1)).
+:-assept(isDeathEater("Альбус Дамблдор",2)).
+:-assept(isAlive("Альбус Дамблдор",2)).
+:-assept(blood("Альбус Дамблдор",2)).
+:-assept(isHuman("Альбус Дамблдор",1)).
+
+:-assept(isMan("Люциус Малфой",1)).
+:-assept(fromGriffindor("Люциус Малфой",2)).
+:-assept(isDeathEater("Люциус Малфой",1)).
+:-assept(isAlive("Люциус Малфой",1)).
+:-assept(blood("Люциус Малфой",1)).
+:-assept(isHuman("Люциус Малфой",1)).
+
+:-assept(isMan("Нарцисса Малфой",2)).
+:-assept(fromGriffindor("Нарцисса Малфой",2)).
+:-assept(isDeathEater("Нарцисса Малфой",1)).
+:-assept(isAlive("Нарцисса Малфой",1)).
+:-assept(blood("Нарцисса Малфой",1)).
+:-assept(isHuman("Нарцисса Малфой",1)).
+
+:-assept(isMan("Полумна Лавгуд"2)).
+:-assept(fromGriffindor("Полумна Лавгуд",2)).
+:-assept(isDeathEater("Полумна Лавгуд",2)).
+:-assept(isAlive("Полумна Лавгуд",1)).
+:-assept(blood("Полумна Лавгуд",2)).
+:-assept(isHuman("Полумна Лавгуд",1)).
+
+:-assept(isMan("Джинни Уизли",2)).
+:-assept(fromGriffindor("Джинни Уизли",1)).
+:-assept(isDeathEater("Джинни Уизли",2)).
+:-assept(isAlive("Джинни Уизли",1)).
+:-assept(blood("Джинни Уизли",1)).
+:-assept(isHuman("Джинни Уизли",1)).
+
+:-assept(isMan("Беллатриса Лестрейндж",2)).
+:-assept(fromGriffindor("Беллатриса Лестрейндж",2)).
+:-assept(isDeathEater("Беллатриса Лестрейндж",1)).
+:-assept(isAlive("Беллатриса Лестрейндж",2)).
+:-assept(blood("Беллатриса Лестрейндж",1)).
+:-assept(isHuman("Беллатриса Лестрейндж",1)).
+
+:-assept(isMan("Игорь Каркаров",1)).
+:-assept(fromGriffindor("Игорь Каркаров",2)).
+:-assept(isDeathEater("Игорь Каркаров",1)).
+:-assept(isAlive("Игорь Каркаров",1)).
+:-assept(blood("Игорь Каркаров",2)).
+:-assept(isHuman("Игорь Каркаров",1)).
+
+:-assept(isMan("Флоренц",1)).
+:-assept(fromGriffindor("Флоренц",2)).
+:-assept(isDeathEater("Флоренц",2)).
+:-assept(isAlive("Флоренц",1)).
+:-assept(blood("Флоренц",5)).
+:-assept(isHuman("Флоренц",2)).
+
+akinator :-
+%вопросы
+write("Загаданный герой мужчина или женщина?"), n1,
+write("1 - Мужчина"), n1,
+write("2 - Женщина"), n1,
+read(IsMan),
+
+write("Персонаж учился или учится на факультете Гриффиндор?"), n1,
+write("1 - Да"), n1,
+write("2 - Нет"), n1,
+read(FromGriffindor),
+
+write("Был ли персонаж пожирателем смерти в прошлом или является им сейчас?"), n1,
+write("1 - Да"), n1,
+write("2 - Нет"), n1,
+read(IsDeathEater),
+
+write("Жив ли персонаж?"), n1,
+write("1 - Да"), n1,
+write("2 - Нет"), n1,
+read(IsAlive),
+
+write("Данный персонаж полукровка? Какова чистота его крови?"), n1,
+write("1 - Чистокровный"), n1,
+write("2 - Полукровка"), n1,
+write("2 - Маглорожденный"), n1,
+write("2 - Магл"), n1,
+write("2 - Иное"), n1,
+read(Blood),
+
+write("Ваш персонаж человек?"), n1,
+write("1 - Да"), n1,
+write("2 - Нет"), n1,
+read(IsHuman),
+
+
+%поиск ответов
+dynamic(result/1),
+forall(
+  (
+   isMan(Character, IsMan),
+   fromGriffindor(Charaster, FromGriffindor),
+   isDeathEater(Character, IsDeathEater),
+   isAlive(Character, IsAlive),
+   blood(Charaster, Blood),
+   isHuman(Charaster, IsHuman)
+  ),
+  (
+   assept(result(Charaster))
+  )
+),
+
+checkResults(IsMan, FromGriffindor, IsDeathEater, IsAlive, Blood, IsHuman),
+abolish(result/1)
+
+checkResults(IsMan, FromGriffindor, IsDeathEater, IsAlive, Blood, IsHuman) :-
+   (
+     result(Charaster),
+     write("Ваш герой - "), write(Charaster), n1,
+     write("1 - Да"), n1,
+     write("2 - Нет"), n1,
+     read(IsTarget),
+     IsTarget=1,
+     true
+   );
+   (
+    write("Я не знаю этого героя:(Кто он?"),
+    read(CharacterNew),
+
+   :-assept(isMan(CharacterNew,IsMan)),
+   :-assept(fromGriffindor(CharacterNew,FromGriffindor)),
+   :-assept(isDeathEater(CharacterNew,IsDeathEather)),
+   :-assept(isAlive(CharacterNew,IsAlive)),
+   :-assept(blood(CharacterNew,Blood)),
+   :-assept(isHuman(CharacterNew,IsHuman)),
+
+   write("В базу записан новый персонаж: "), write(CharacterNew), n1,
+   
+   saveToFile('UpdatedBase.pl'),
+
+   write("База сохранена в файл UpdatedBase.pl. Файл находится или в папке пользователя, или, если пролог запущен через консоль, в "),
+   write("текущей папке консоли."), nl,
+   true
+    ).
+saveToFile(File) :-
+tell(File),
+
+write("dynamic isMan/2."), n1,
+write("dynamic fromGriffindor/2"), n1,
+write("dynamic isDeathEater/2"), n1,
+write("dynamic isAlive/2"), n1,
+write("dynamic blood/2"), n1,
+write("dynamic isHuman/2"), n1,
+n1,
+
+
+
