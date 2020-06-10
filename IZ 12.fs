e260070da0b7cb800e9aedf106d15b4fc59ce812
EkaterinaKozel <66666949+EkaterinaKozel@users.noreply.github.com>
2020-06-10 11:17:20 +0300

    Create IZ 12.fs

---
+++ IZ 12.fs
@@ -0,0 +1,17 @@
+﻿open System
+let rec insert i = function
+| h::t -> min h i::(insert (max h i) t)
+| _ ->[i]
+
+let insertionSort l = List.foldBack insert l []
+
+[<EntryPoint>]
+let main argv =
+    let list = [2; 5; 4; 9; 7; 3; 10; 8; 6; 1 ]
+    let result= insertionSort list
+
+    printfn "Изначальный список:      %A" list
+    printfn "Отсортированный список:  %A" result
+
+    System.Console.Read()
+    0//
